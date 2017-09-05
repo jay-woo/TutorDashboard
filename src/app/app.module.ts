11cff57acae1a745625a646b986eb5edce67a128
Jay Woo <wooj@Jays-MacBook-Pro.local>
2017-09-05 15:50:36 -0400

    Added basic components + routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// Component imports
 import { AppComponent } from './app.component';
+import { ClientDashboardComponent } from './components/client-dashboard/client-dashboard.component';
+import { AppointmentDashboardComponent } from './components/appointment-dashboard/appointment-dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
+import { AppointmentDetailsComponent } from './components/appointment-details/appointment-details.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { AddAppointmentComponent } from './components/add-appointment/add-appointment.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { EditAppointmentComponent } from './components/edit-appointment/edit-appointment.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+// Routes
+const appRoutes:Routes = [
+  {path: '', component: AppointmentDashboardComponent},
+  {path: 'clients', component: ClientDashboardComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientDashboardComponent,
+    AppointmentDashboardComponent,
+    ClientsComponent,
+    AppointmentsComponent,
+    AppointmentDetailsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    AddAppointmentComponent,
+    EditClientComponent,
+    EditAppointmentComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/appointment-dashboard/appointment-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-dashboard',
+  templateUrl: './appointment-dashboard.component.html',
+  styleUrls: ['./appointment-dashboard.component.css']
+})
+export class AppointmentDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
